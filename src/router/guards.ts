b2db7771f1ff965f1466115f0abee6bc8a3fa9eb
cf <1406211263@qq.com>
2020-12-10 18:52:00 +0800

    取消 页面跳转 登录限制

--- src/router/guards.ts
+++ src/router/guards.ts
@@ -1,10 +1,9 @@
-import Vue from 'vue'
 import { Route } from 'vue-router/types/router'
 import router from './index'
 import { UserStoreModule } from '@/store/modules/userStore'
 
 import NProgress from 'nprogress' // 加载进度条
-import { getXStreamId, getStreamIdExpireTime } from '../utils/sessionTool' // get token from cookie
+import { getStreamIdExpireTime } from '../utils/sessionTool' // get token from cookie
 import * as UserApi from '../api/userApi'
 import '@assetsDir/styles/nprogress.less' // 进度条颜色
 
@@ -14,7 +13,6 @@ const whiteList = ['/', '/login', '/auth-redirect', '/401', '/404'] // 白名单
 
 router.beforeEach(async (to: Route, from: Route, next: any) => {
   NProgress.start() // 读取进度条
-  const hasXStreamId = getXStreamId() // 获取token
   // 没有过期时的操作
   async function noExpire () {
     if (to.path === '/login') {
@@ -34,32 +32,19 @@ router.beforeEach(async (to: Route, from: Route, next: any) => {
     }
   }
 
-  function goLogin () {
-    if (whiteList.indexOf(to.path) !== -1) {
-      next()
-    } else {
-      next(`/login?redirect=${to.path}`)
-      NProgress.done()
+  // 下次的过期时间
+  const expireTime = getStreamIdExpireTime() * 1000
+  const nowTime = new Date().getTime()
+  const discrepancyTime = expireTime - nowTime
+  if (discrepancyTime > 0) {
+    // 过期时间小于1小时续上
+    if (discrepancyTime < 60 * 60 * 1000) {
+      await UserApi.userExpire()
     }
-  }
-
-  if (hasXStreamId) {
-    // 下次的过期时间
-    const expireTime = getStreamIdExpireTime() * 1000
-    const nowTime = new Date().getTime()
-    const discrepancyTime = expireTime - nowTime
-    if (discrepancyTime > 0) {
-      // 过期时间小于1小时续上
-      if (discrepancyTime < 60 * 60 * 1000) {
-        await UserApi.userExpire()
-      }
-      noExpire()
-    } else if (discrepancyTime < 0) {
-      UserApi.logout()
-      goLogin()
-    }
-  } else {
-    goLogin() // 没有xStreamId
+    noExpire()
+  } else if (discrepancyTime < 0) {
+    UserApi.logout()
+    next()
   }
 })
 
