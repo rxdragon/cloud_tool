73049aa6e924845e70a8e522bc5c86ce898a3ca8
cf <1406211263@qq.com>
2021-02-01 17:22:49 +0800

    更新版本

--- src/service-worker.js
+++ src/service-worker.js
@@ -9,7 +9,7 @@ if (workbox) {
 // 设置缓存前缀和后缀，请根据实际项目名修改
 workbox.core.setCacheNameDetails({
   prefix: 'cloud_tool',
-  suffix: 'v1.0.6'
+  suffix: 'v1.0.7'
 })
 
 // have our sw update and control a web page as soon as possible.
