7b16ed27b58586c509952b8f9851ed028a64d3a0
rxl <r-x-dragon@foxmail.com>
2021-04-22 18:45:12 +0800

    修复了轮播图的bug
    - 重新换了v-window组件，更有交互性
    - 因为无法重新绑定v-window组件的按钮事件，自己另加了切换图片的两个按钮
    - 提高了用户使用的交互性：在第一张和最后一张图片时，相应的切换按钮会隐藏。

--- src/components/ImgPreview/ImgPreview.vue
+++ src/components/ImgPreview/ImgPreview.vue
@@ -3,34 +3,37 @@
     <transition name="main">
       <div id="ImgPreview" ref="ImgPreview" v-show="viewerVisibleShow">
         <!-- 预览的图片区域-->
-        <v-carousel height="100%" hide-delimiters hide-delimiter-background show-arrows-on-hover>
-          <v-carousel-item v-for="(item, i) in imgDataList" :key="i">
+        <v-window class="v-window" :show-arrows="false" dark :touch="switchImg">
+          <div v-show="imgIndex !== 0" class="prev-img">
+            <v-icon large color="blue darken-2" @click="prevImg">
+              mdi-arrow-left-circle
+            </v-icon>
+          </div>
+          <div v-show="imgIndex !== (imgDataListLength - 1)" class="next-img">
+            <v-icon large color="blue darken-2" @click="nextImg">
+              mdi-arrow-right-circle
+            </v-icon>
+          </div>
+          <v-window-item class="v-window-item" v-for="(item, i) in imgDataList" :key="i">
             <v-sheet height="100%">
-              <v-row class="fill-height" align="center" justify="center">
+              <v-row class="v-row" align="center" justify="center">
                 <div class="img-display">
                   <!--加载动画-->
                   <div v-if="loading">
                     <v-progress-circular indeterminate size="64"></v-progress-circular>
                   </div>
-                  {{ item.title || imgName}}
-                  <v-img
-                    contain
-                    @click="visible()"
-                    max-height="900px"
-                    class="img"
-                    ref="img"
-                    :src="item.url"
-                  ></v-img>
+                  {{ imgName }}
+                  <v-img contain @click="visible()" max-height="900px" class="img" ref="img" :src="imgSrc"></v-img>
                   <div class="download-img">
-                    <v-icon large color="blue darken-2" @click="download(item)">
+                    <v-icon large color="blue darken-2" @click="download(imgSrc)">
                       mdi-download
                     </v-icon>
                   </div>
                 </div>
               </v-row>
             </v-sheet>
-          </v-carousel-item>
-        </v-carousel>
+          </v-window-item>
+        </v-window>
       </div>
     </transition>
   </div>
@@ -45,6 +48,10 @@ export default class ImgPreview extends Vue {
   @Prop() public imgDataList!: any[]
   @Prop() public imgIndex!: number
 
+  public switchImg: object = {
+    left: this.nextImg,
+    right: this.prevImg,
+  }
   public imgSrc: string = '' // 预览图片的src
   public imgName: string = 'loading error' // 图片名
   public imgDataListLength!: number // 数组的长度
@@ -113,13 +120,69 @@ export default class ImgPreview extends Vue {
   }
 
   // 下载图片
-  public download (item: any) {
-    window.location.href = `${item.url}?attname=`
+  public download (src: any) {
+    window.location.href = `${src}?attname=`
   }
 
   // 改变loading状态
   public changeLoading (status: boolean) {
     this.loading = status
+  }
+
+  //  上一张图
+  public prevImg () {
+    if (this.imgI === 0) {
+      return
+    } else {
+      this.cleanData()
+      this.changeLoading(true)
+      this.imgDataListLength = this.imgData.length
+      this.imgI -= 1
+      const indexNext: number = this.imgI
+      this.init(indexNext - 1)
+    }
+  }
+
+  // 下一张图
+  public nextImg () {
+    let imgDataLength: number = this.imgData.length
+    if (this.imgI === imgDataLength - 1) {
+      return
+    } else {
+      this.cleanData()
+      this.changeLoading(true)
+      this.imgDataListLength = this.imgData.length
+      this.imgI += 1
+      const indexNext: number = this.imgI
+      this.init(indexNext + 1)
+    }
+  }
+
+  // 清除数据
+  public cleanData () {
+    const arr = this.$data
+    for (const key in arr) {
+      if (arr.hasOwnProperty(key)) {
+        if (typeof arr[key] !== 'object') {
+          if (typeof arr[key] === 'string' && key !== 'imgName') {
+            this.changeData(key, 'string', this)
+          }
+          if (key === 'imgName') {
+            this.changeData(key, 'imgName', this)
+          }
+        }
+      }
+    }
+  }
+
+  // 修改值
+  public changeData (key: any, type: any, that: any) {
+    if (type === 'string') {
+      that[key] = ''
+    }
+    if (type === 'number') {
+      that[key] = 0
+    }
   }
 
   // 设置预览图片
@@ -193,23 +256,61 @@ export default class ImgPreview extends Vue {
   background: rgb(0, 0, 0, 0.55);
 }
 
-.img-version{
+.v-window {
+  height: 100%;
+}
+
+.v-window .prev-img{
+  display: none;
+}
+
+.v-window:hover .prev-img{
+  display: block;
+  z-index: 2030;
+  position: absolute;
+  padding-left: 10px;
+  left: 0;
+  top: 50%;
+}
+
+.v-window .next-img{
+  display: none;
+}
+
+.v-window:hover .next-img{
+  display: block;
+  z-index: 2030;
+  position: absolute;
+  padding-right: 10px;
+  right: 0;
+  top: 50%;
+}
+
+.v-window-item {
+  height: 100%;
+}
+
+.v-row {
+  height: 100%;
+}
+
+.img-version {
   text-align: center;
   font-weight: 400;
   font-size: 17px;
 }
 
-.img-display{
-  width:auto;
-  height:auto;
-  max-width:100%;
-  max-height:100%;
+.img-display {
+  width: auto;
+  height: auto;
+  max-width: 100%;
+  max-height: 100%;
   text-align: center;
   font-size: 17px;
   font-weight: 400;
 }
 
-.download-img{
+.download-img {
   z-index: 2019;
   padding-top: 10px;
   display: block;
